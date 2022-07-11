cb460758605a3f3b5c25de8a62469627c8417fe0
gl2486 <108635800+gl2486@users.noreply.github.com>
2022-07-11 00:21:10 -0400

    user input prompt and simulating computer selection

--- game.py
+++ game.py
@@ -3,3 +3,20 @@
 # this is the "game.py" file...
 
 print("Rock, Paper, Scissors, Shoot!")
+
+
+# user action input 
+
+user_action = input("Please choose either 'rock', 'paper' or 'scissors': ")
+
+
+# list of possible actions
+# computer random selection..
+
+import random
+
+possible_actions = ["rock", "paper", "scissors"]
+computer_action = random.choice(possible_actions)
+
+print(f"\nYou chose: {user_action} \nComputer chose: {computer_action}\n")
+
